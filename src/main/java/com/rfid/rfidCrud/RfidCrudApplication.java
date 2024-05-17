0d500c88545c0ab3ddacb9da72581d065c87eb68
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2024-05-17 15:03:47 +0530

    first commit

---
+++ src/main/java/com/rfid/rfidCrud/RfidCrudApplication.java
@@ -0,0 +1,13 @@
+package com.rfid.rfidCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfid/rfidCrud/RfidCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfid.rfidCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfidCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
